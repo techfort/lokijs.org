9ee4c301980e7b55438203606b197dbe794dddc0
Joe Minichino <joe.minichino@gmail.com>
2014-12-17 15:29:59 +0000

    deps

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var connect = require('connect');
-connect.createServer(connect.static(__dirname)).listen(80);
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
 
+// Serve up public/ftp folder
+var serve = serveStatic(__dirname, {
+  'index': ['index.html']
+})
+
+// Create server
+var server = http.createServer(function (req, res) {
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(3000)
